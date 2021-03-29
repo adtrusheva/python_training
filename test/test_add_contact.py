4b889aadc4bc212a2c1444230da9b187111a4b89
Alena Trusheva <trusheva@self.team>
2021-03-30 00:21:58 +0500

    Модифицирован тест по изменению контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -36,31 +36,5 @@ def test_add_contact(app):
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create(Contact(
-        photo='',
-        firstname='',
-        middlename='',
-        lastname='',
-        nickname='',
-        title='',
-        company='',
-        address='',
-        home='',
-        mobile='',
-        work='',
-        fax='',
-        email='',
-        email2='',
-        email3='',
-        homepage='',
-        bday='',
-        bmonth='',
-        byear='',
-        aday='',
-        amonth='',
-        ayear='',
-        address2='',
-        phone2='',
-        notes=''
-    ))
+    app.contact.create(Contact())
     app.session.logout()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,33 +2,19 @@
 from model.contact import Contact
 
 
-def test_edit_first_contact(app):
+def test_edit_contact_firstname(app):
     app.session.login(username="admin", password="secret")
-    app.contact.edit_first_contact(Contact(
-        photo='contact_photo_edit.png',
-        firstname='Igor',
-        middlename='Victirivich',
-        lastname='Pushkin',
-        nickname='geek',
-        title='pure',
-        company='OutStaf',
-        address='Moscow, Red Proletarskiy',
-        home='92',
-        mobile='983726000',
-        work='9284730000',
-        fax='8473700000',
-        email='qwertys1@mail.ru',
-        email2='qwertys2mail.ru',
-        email3='qwertys3@mail.ru',
-        homepage='wasd.com',
-        bday='25',
-        bmonth='June',
-        byear='1995',
-        aday='9',
-        amonth='May',
-        ayear='1999',
-        address2='Moscow, Serbryanicheskaya Naberezhnaya',
-        phone2='928473001',
-        notes='python'
-    ))
+    app.contact.edit_first_contact(Contact(firstname='Petr'))
+    app.session.logout()
+
+
+def test_edit_contact_middlename(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact(middlename='Vasilevich'))
+    app.session.logout()
+
+
+def test_edit_contact_lastname(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact(lastname='Durov'))
     app.session.logout()
