53180d718395ef99d438e08c92dbedac6c5a85c5
Alena Trusheva <trusheva@self.team>
2021-03-29 22:42:36 +0500

    Модифицирован тест по изменению группы

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,12 @@
 from model.group import Group
 
 
-def test_edit_first_group(app):
+def test_edit_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="566300", header="00000d", footer="999999w"))
+    app.group.edit_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_edit_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="New header"))
     app.session.logout()
