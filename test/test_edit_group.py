0fb38de5d2949d8d46fcb5c33ba3d9a26a9481ab
Alena Trusheva <trusheva@self.team>
2021-03-30 16:11:24 +0500

    Реализованы выполнения предусловий в тестах по удаллению и модификации групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8 -*-
 from model.group import Group
 
 
+def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
+    app.group.edit_first_group(Group(name="New1 group", footer="New1 footer", header="New1 header"))
+
+
 def test_edit_group_name(app):
-    app.group.edit_first_group(Group(name="New group"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="02"))
+    app.group.edit_first_group(Group(name="New Test"))
 
 
 def test_edit_group_header(app):
-    app.group.edit_first_group(Group(header="New header"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="01"))
+    app.group.edit_first_group(Group(header="New3 header"))
