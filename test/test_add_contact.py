4d9365d90139e47b25b42a514d7334e44b3be93b
Alena Trusheva <trusheva@self.team>
2021-03-30 10:56:56 +0500

    Осуществлена валидация фикстуры перед каждым тестом

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,6 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(
         photo='contact_photo.jpg',
         firstname='Fedor',
@@ -31,10 +30,7 @@ def test_add_contact(app):
         phone2='928473627',
         notes='port'
     ))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact())
-    app.session.logout()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,18 +3,12 @@ from model.contact import Contact
 
 
 def test_edit_contact_firstname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(firstname='Petr'))
-    app.session.logout()
 
 
 def test_edit_contact_middlename(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(middlename='Vasilevich'))
-    app.session.logout()
 
 
 def test_edit_contact_lastname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(lastname='Durov'))
-    app.session.logout()
