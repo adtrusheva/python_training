2a0e0ffbc4ac1e93aa46cc85c142a94ecc7dbf78
Alena Trusheva <trusheva@self.team>
2021-04-11 15:45:21 +0500

    Созданы тесты для проверки телефонов контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,6 +1,7 @@
 from model.contact import Contact
 from selenium.webdriver.support.ui import Select
 import os
+import re
 
 
 class ContactHelper:
@@ -106,7 +107,10 @@ class ContactHelper:
                 id = element.find_element_by_name("selected[]").get_attribute('value')
                 firstname = element.find_element_by_xpath('td[3]').text
                 lastname = element.find_element_by_xpath('td[2]').text
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+                all_phones = element.find_element_by_xpath('td[6]').text.splitlines()
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  home=all_phones[0], mobile=all_phones[1],
+                                                  work=all_phones[2], phone2=all_phones[3]))
         return list(self.contact_cache)
 
     def select_contact_by_index(self, index):
@@ -126,12 +130,45 @@ class ContactHelper:
 
     def edit_contact_by_index(self, new_contact_data, index):
         wd = self.app.wd
-        self.go_to_home_page()
-        # submit edition
-        wd.find_element_by_xpath("(//img[@alt='Edit'])[" + str(index + 1) + "]").click()
+        self.open_contact_to_edit_by_index(index)
         # edition contact form
         self.fill_form(new_contact_data)
         # submit contact edition
         wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
         self.go_to_home_page()
         self.contact_cache = None
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.go_to_home_page()
+        wd.find_element_by_xpath("(//img[@alt='Edit'])[" + str(index + 1) + "]").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.go_to_home_page()
+        wd.find_element_by_xpath("(//img[@alt='Details'])[" + str(index + 1) + "]").click()
+
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element_by_name("firstname").get_attribute("value")
+        lastname = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home = wd.find_element_by_name("home").get_attribute("value")
+        mobile = wd.find_element_by_name("mobile").get_attribute("value")
+        work = wd.find_element_by_name("work").get_attribute("value")
+        phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id,
+                       home=home, mobile=mobile, work=work, phone2=phone2)
+
+    def get_contact_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home = re.search("H: (.*)", text).group(1)
+        mobile = re.search("M: (.*)", text).group(1)
+        work = re.search("W: (.*)", text).group(1)
+        phone2 = re.search("P: (.*)", text).group(1)
+        return Contact(home=home, mobile=mobile, work=work, phone2=phone2)
+
