58fd885444a0c15208b28986acebda9d3e163c6c
Alena Trusheva <trusheva@self.team>
2021-04-11 22:40:08 +0500

    Выполнена параметризация теста на создание контактов

--- model/contact.py
+++ model/contact.py
@@ -61,7 +61,34 @@ class Contact:
         self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s" % (
+            self.id,
+            self.firstname,
+            self.lastname,
+            self.middlename,
+            self.lastname,
+            self.nickname,
+            self.title,
+            self.company,
+            self.address,
+            self.home,
+            self.mobile,
+            self.work,
+            self.fax,
+            self.email,
+            self.email2,
+            self.email3,
+            self.homepage,
+            self.bday,
+            self.bmonth,
+            self.byear,
+            self.aday,
+            self.amonth,
+            self.ayear,
+            self.address2,
+            self.phone2,
+            self.notes
+        )
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,46 +1,92 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_address(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_number(maxlen):
+    numeral = string.digits
+    return [random.choice(numeral) for i in range(random.randrange(maxlen))]
+
+
+def random_email(prefix, maxlen):
+    mail = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(mail) for i in range(random.randrange(maxlen))]) + '@mail.example'
+
+
+def random_website(prefix, maxlen):
+    site = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(site) for i in range(random.randrange(maxlen))]) + '.com'
+
+
+def random_month():
+    month_list = [
+        "January"
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+
+    return random.choice(month_list)
+
+
+def random_year():
+    return str(random.randint(1900, 2020))
+
+
+def random_day():
+    return str(random.randint(1, 31))
+
+
+def random_photo():
+    photo_list = [
+        "contact_photo.jpg",
+        "contact_photo_edit.png",
+    ]
+
+    return random.choice(photo_list)
+
+
+testdata = [Contact(firstname="", lastname="", middlename="")] \
+           + \
+           [
+               Contact(firstname=random_string("firstname", 8), lastname=random_string("lastname", 8),
+                       middlename=random_string("middlename", 8), photo=random_photo(),
+                       nickname=random_string("nickname", 5),
+                       title=random_string("title", 4), company=random_string("company", 10),
+                       address=random_address("address", 20),
+                       home=random_number(9), mobile=random_number(9), work=random_number(9),
+                       fax=random_number(9), email=random_email("email", 8), email2=random_email("email2", 8),
+                       email3=random_email("email3", 8), homepage=random_website("homepage", 8), bday=random_day(),
+                       bmonth=random_month(), byear=random_year(), aday=random_day(), amonth=random_month(),
+                       ayear=random_year(), address2=random_address("address", 20), phone2=random_number(9),
+                       notes=random_string("notes", 7))
+               for i in range(5)
+           ]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(
-        photo='contact_photo.jpg',
-        firstname='Fedor',
-        middlename='Ivanovich',
-        lastname='Truhin',
-        nickname='pups',
-        title='funny',
-        company='Self',
-        address='Moscow, Color Bulvar',
-        home='34',
-        mobile='983726475',
-        work='9284737264',
-        fax='8473748394',
-        email='4662e@mail.ru',
-        email2='63g73mail.ru',
-        email3='fgr63hf@mail.ru',
-        homepage='terwye.com',
-        bday='11',
-        bmonth='May',
-        byear='1991',
-        aday='5',
-        amonth='June',
-        ayear='1995',
-        address2='Moscow, Red Bulvar',
-        phone2='928473627',
-        notes='port'
-    )
-    app.contact.create(contact)
-    assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname='', lastname='')
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
